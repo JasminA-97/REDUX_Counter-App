badce8bf36a7ef4182264c9e0d7f12f8c4d3d788
JasminA-97 <21mp1828@rit.ac.in>
2024-05-18 12:04:05 +0530

    REDUX-Counter_App

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
+Counter-App using REDUX
+------------------------
+
+-REDUX : A JavaScript library for predictable and maintanable global state management 
+
+ <!--install react-redux and redux toolkit  -->
+ >>npm i @reduxjs/toolkit react-redux 
+
+ 
+- configureStore() is of type obj ==>configureStore({})
+- reducer is inside configureStore, where state is stored 
+    -only reducer can update the state (configure store and reducer is used bcz component cannot directly update the state)
+
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,20 @@
+
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, reset } from '../redux/counterSlice'
 
 const Counter = () => {
+    const dispatch = useDispatch ()
+    const {count} = useSelector(state => state.counterReducer)
   return (
     <div>
        <h1 className='text-center'>Counter App</h1> 
        <div style={{width:'600px'}} className='border p-5 rounded-5 mt-5'>
-        <h1 style={{fontSize:'80px'}} className='text-center'>100</h1>
+        <h1 style={{fontSize:'80px'}} className='text-center'>{count}</h1>
         <div className="d-flex justify-content-around mt-3 align-items-center w-100">
-            <button className='btn btn-warning'>DECREMENT</button>
-            <button className='btn btn-danger'>RESET</button>
-            <button className='btn btn-success'>INCREMENT</button>
+            <button onClick={()=>dispatch(decrement())}  className='btn btn-warning'>DECREMENT</button>
+            <button onClick={()=>dispatch(reset())} className='btn btn-danger'>RESET</button>
+            <button onClick={()=>dispatch(increment())}  className='btn btn-success'>INCREMENT</button>
         </div>
         <div className="d-flex justify-content-between align-items-center mt-5 w-100">
             <input type="text" className="form-control" placeholder='Incremented Counter Amount' />
